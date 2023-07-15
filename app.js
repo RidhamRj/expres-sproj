c5e6ac794f05ca9a59edc7db9ca72eea73ced63f
yash-25log <singhal2003yash@gmial.com>
2023-07-15 15:23:34 +0530

    bug

--- app.js
+++ app.js
@@ -34,9 +34,7 @@ app.post("/", (req, res) => {
   const request = https.request(url, option, (response) => {
     if(response.statusCode===200){
       res.sendFile(__dirname+'/success.html')
-    }else if(response.statusCode===401){
-      res.sendFile(__dirname+'/401.html')
-      }else{
+    }else{
       res.sendFile(__dirname+'/failure.html')
 
     }
